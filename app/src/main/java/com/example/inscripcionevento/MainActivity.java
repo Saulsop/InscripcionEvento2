7bc9ae913aa0982a6872c412d40cb92e1bdb848d
Saúlsop <sauljesusperezflores@gmail.com>
2025-01-30 15:47:38 -0600

    Toast commit

--- app/src/main/java/com/example/inscripcionevento/MainActivity.java
+++ app/src/main/java/com/example/inscripcionevento/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.inscripcionevento;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+     EditText txtNombre;
+     EditText txtEmail;
+     RadioGroup rgAsistencia;
+     RadioButton rbSi;
+     RadioButton rbNo;
+     Button btnEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        txtNombre = findViewById(R.id.txtNombre);
+        txtEmail = findViewById(R.id.txtEmail);
+        rgAsistencia = findViewById(R.id.RadioGroup);
+        rbSi = findViewById(R.id.siRadioButton);
+        rbNo = findViewById(R.id.noRadioButton);
+        btnEnviar = findViewById(R.id.btnEnviar);
+
+        btnEnviar.setOnClickListener(v -> {
+            String nombre = txtNombre.getText().toString();
+            String email = txtEmail.getText().toString();
+            String asistencia = rbSi.isChecked() ? "Sí" : "No";
+
+            if (asistencia.equals("Sí")) {
+                String mensaje = "¡Registro exitoso!\n" +
+                        "Bienvenido: " + nombre + "\n";
+                Toast.makeText(MainActivity.this, mensaje, Toast.LENGTH_LONG).show();
+            } else {
+                String mensaje = "¡Una pena que no vendrás, " + nombre + "!";
+                Toast.makeText(MainActivity.this, mensaje, Toast.LENGTH_LONG).show();
+            }
+
+
+        });
+
     }
 }
